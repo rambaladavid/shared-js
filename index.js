f95c4b21ff8010a7378217892ddfd1f8f5261964
Dávid Rambala <rambala.david@gmail.com>
2023-04-06 20:23:58 +0200

    Test committing from my laptop.

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ import Invoice from "./Model/Invoice.js"
 
 let invoice = new Invoice(10, "david");
 let invoice2 = new Invoice(11, "daavid");
+
+// add a comment to test committing and signing from my laptop
